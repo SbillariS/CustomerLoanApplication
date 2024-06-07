9a35074047b32b75cca315a6f6a8ff4d693a5801
PRIYAVAJ <priyankavaj1006@gmail.com>
2024-06-07 20:44:33 +0530

    ok

--- CustomerLoanApplication/src/main/java/com/carlelo/customerservice/controller/loanApplicantController.java
+++ CustomerLoanApplication/src/main/java/com/carlelo/customerservice/controller/loanApplicantController.java
@@ -4,21 +4,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.carlelo.customerservice.model.LoanApplicant;
+import com.carlelo.customerservice.model.CustomerAddress;
+import com.carlelo.customerservice.model.MedicalDetails;
+import com.carlelo.customerservice.servicei.CustomerAddressServiceI;
 import com.carlelo.customerservice.servicei.LoanApplicantServiceI;
+import com.carlelo.customerservice.servicei.MedicalDetailsServiceI;
+
 
 @RestController
 public class loanApplicantController 
 {
-	@Autowired LoanApplicantServiceI loan;
+	@Autowired
+	LoanApplicantServiceI loan;
 	
-	@PostMapping("/addCustomerDetails")
-	public ResponseEntity<LoanApplicant> addCustomerDetails(@RequestPart("customerJson") String customerdetails)
+	@Autowired
+	MedicalDetailsServiceI mds;
+	
+	@Autowired
+	CustomerAddressServiceI customeradrr;
+	
+	@PostMapping("/addMedical")
+	public ResponseEntity<MedicalDetails> saveMedicalDetails(@RequestBody MedicalDetails md)
 	{
-		LoanApplicant la=loan.addCustomerDetails(customerdetails);
-		return new ResponseEntity<LoanApplicant>(la,HttpStatus.CREATED);	
+		MedicalDetails md1=mds.saveMedicalDetails();
+		
+		return new ResponseEntity<MedicalDetails>(md1,HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/addcustomer")
+	public ResponseEntity<CustomerAddress> saveCustomerAddress(@RequestBody CustomerAddress cad)
+	{
+		CustomerAddress cad1 =customeradrr.saveCustomerAddress();
+		
+		return new ResponseEntity<CustomerAddress>(cad1,HttpStatus.CREATED);
 	}
 }
