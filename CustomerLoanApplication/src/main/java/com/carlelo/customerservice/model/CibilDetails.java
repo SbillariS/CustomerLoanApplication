6db313f9f7384f6ad738169344d21f4f69d45936
Aboli3872 <aboli51996@gmail.com>
2024-06-10 15:31:54 +0530

    created ledger controller and added put method to it.

--- CustomerLoanApplication/src/main/java/com/carlelo/customerservice/model/CibilDetails.java
+++ CustomerLoanApplication/src/main/java/com/carlelo/customerservice/model/CibilDetails.java
@@ -1,10 +1,14 @@
 package com.carlelo.customerservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class CibilDetails 
 {
+	@Id
 	    private String cibilId;
 	    private int cibilScore;
 	    private String remark;
